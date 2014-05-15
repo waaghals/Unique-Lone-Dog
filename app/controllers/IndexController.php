e3b243f43a1fc20421002d61cdfcf7f5df9cb42a
Waaghals <patrick.stroef@gmail.com>
2014-05-15 13:51:28 +0200

    Add machine tag model

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -3,13 +3,19 @@
 namespace UniqueLoneDog\Controllers;
 
 use Phalcon\Mvc\Controller;
+use UniqueLoneDog\Models\MachineTag;
 
 class IndexController extends Controller
 {
 
     public function indexAction()
     {
-        
+
+    }
+
+    public function testAction()
+    {
+        $tag = new MachineTag();
     }
 
 }
